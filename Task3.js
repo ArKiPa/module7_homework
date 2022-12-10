94058555536805a714e07615c743331f566cc1b3
ArKiPa <arslanov.kirill@mail.ru>
2022-12-10 21:04:19 +0500

    i have done homework5

---
+++ Task3.js
@@ -0,0 +1,9 @@
+﻿/*Дана строка.Необходимо вывести в консоль перевёрнутый вариант.Например, "Hello" -> "olleH".*/
+
+let x = "Hello";
+let SumStr = "";
+for (let i = 1; i <= x.length; i++) {
+    SumStr = SumStr + x.slice(x.length - i, x.length - i + 1);
+}
+console.log(x);
+console.log(SumStr);
---
+++ Task8.js
@@ -0,0 +1,14 @@
+﻿/*Создайте произвольный массив Map.Получите его ключи и выведите в консоль все значения в виде «Ключ — Х, значение — Y».*/
+
+let map = new Map();
+map.set("red", "One");
+map.set("orange", "Two");
+map.set("yellow", "Three");
+map.set("green", "Four");
+map.set("blue", "Five");
+map.set("indigo", "Six");
+map.set("violet", "Seven");
+
+for (let name of map.keys()) {
+    console.log("Ключ — " + name + ", " + " значение — " + map.get(name));
+}
